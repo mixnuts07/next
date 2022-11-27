ebeb7292acd8b531e43c970af9e1948291342eb1
mixnuts07 <french0719@gmail.com>
2022-11-28 01:15:08 +0900

    add jest

--- pages/api/weather/weather.tsx
+++
@@ -1,9 +0,0 @@
-const Weather = async (req, res) => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${process.env.WEATHER_API_KEY}&lang=ja`
-  );
-  const data = await response.json();
-  res.status(200).json(data);
-};
-
-export default Weather;
